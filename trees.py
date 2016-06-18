3eb97bb9cf0e6c54246b9651d7d036db396a0b0c
282886636@qq.com <282886636@qq.com>
2016-06-18 21:14:22 +0800

    2

--- trees.py
+++ trees.py
@@ -1,2 +1,14 @@
 from math import log
-def
+def calcShannonEnt=len(dataSet):
+    numEntries=len(dataSet)
+    labelCounts={}
+    for featVec in dataSet:
+        currentLabel=featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel]=0
+        labelCounts[currentLabel]+=1
+    shannonEnt=0.0
+    for key in labelCounts:
+        prob=float(labelCounts[key])/numEntries
+        shannonEnt-=prob*log(prob,2)
+    return shannonEnt
