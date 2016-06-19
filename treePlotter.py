a478c87ce874374b161fbe4701831b80dbd04f3b
282886636@qq.com <282886636@qq.com>
2016-06-19 20:11:08 +0800

    DD

--- treePlotter.py
+++ treePlotter.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from ipdb import set_trace
 
 
 decisionNode=dict(boxstyle="sawtooth",fc="0.8")
@@ -14,6 +15,7 @@ def createPlot():
     fig=plt.figure(1,facecolor='white')
     fig.clf()
     createPlot.axl=plt.subplot(111,frameon=False)
-    plotNode("juecejiedian",(0.5,0,1),(0,1,0.5),decisionNode)
-    plotNode('yezijiedian',(0.8,0.1),(0.3,0.8),leafNode)
+    set_trace()
+    plotNode("juecejiedian",(0.5,0.1),(0.1,0.5),decisionNode)
+    plotNode("yezijiedian",(0.8,0.1),(0.3,0.8),leafNode)
     plt.show()
