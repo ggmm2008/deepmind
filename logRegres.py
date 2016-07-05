e561957143e90ca0f22c38453b38f1ed5d8b787e
282886636@qq.com <282886636@qq.com>
2016-07-05 21:48:32 +0800

    0705

--- logRegres.py
+++ logRegres.py
@@ -64,35 +64,23 @@ def stocGradAscent0(dataMatrix,classLabels):
     m,n=np.shape(dataMatrix)
     alpha=0.01
     weights=np.ones(n)
+    xPlot=np.zeros((m,4))
     #set_trace()##################
     for i in range(m):
         hd=sigmoid(sum(dataMatrix[i]*weights))
         error=classLabels[i]-hd
-        weights=weights+list(alpha*error*np.array(dataMatrix[i])) //nainaide 
-    return weights
+        weights=weights+list(alpha*error*np.array(dataMatrix[i])) #nainaide 
+        #set_trace()####################
+        xPlot[i][1:4]=weights
+        xPlot[i][0]=i
+    return weights,xPlot
 
 
-def stocGradAscent1(dataMatrix,classLabels,numIter=150):
+def stocGradAscent2(dataMatrix,classLabels,numIter=250):
     m,n=np.shape(dataMatrix)
     #alpha=0.01
     weights=np.ones(n)
-    #set_trace()##################
-    for jd in range(numIter): 
-        dataIndex=range(m)
-        for i in range(m):
-            alpha=4/(1.0+jd+i)+0.01
-            randIndex=int(np.random.uniform(0,len(dataIndex)))
-           # print '%s/%s,%s/%s,randIndex=%s' % (str(jd),str(numIter),str(i),str(m),str(randIndex))+'\n'
-            hd=sigmoid(sum(dataMatrix[randIndex]*weights))
-            error=classLabels[randIndex]-hd
-            weights=weights+list(alpha*error*np.array(dataMatrix[randIndex]))
-            del(dataIndex[randIndex])           
-    return weights
-
-def stocGradAscent2(dataMatrix,classLabels,numIter=150):
-    m,n=np.shape(dataMatrix)
-    #alpha=0.01
-    weights=np.ones(n)
+    xPlot=np.zeros((numIter,4))
     #set_trace()###################
     for jd in range(numIter): 
         dataIndex=range(m)
@@ -103,5 +91,45 @@ def stocGradAscent2(dataMatrix,classLabels,numIter=150):
             hd=sigmoid(sum(dataMatrix[randIndex]*weights))
             error=classLabels[randIndex]-hd
             weights=weights+list(alpha*error*np.array(dataMatrix[randIndex]))
-            del(dataIndex[randIndex])           
-    return weights
+            del(dataIndex[randIndex])
+        xPlot[jd][1:4]=weights
+        xPlot[jd][0]=jd           
+    return weights,xPlot
+
+
+def plotdiedai(xPlot):
+    import matplotlib.pyplot as plt
+    import pandas as pd   
+   
+    #x0/x1   
+    fig=plt.figure() 
+    ax=fig.add_subplot(111)
+    #set_trace()################
+    x=(pd.DataFrame(xPlot)).ix[:,0]
+    y=(pd.DataFrame(xPlot)).ix[:,1]
+    ax.scatter(x,y,s=30,c='green')
+    ax=plt.plot(x,y)    
+    plt.xlabel('x0');plt.ylabel('x1');
+    plt.show()
+
+    #x0/x2
+    fig=plt.figure() 
+    ax=fig.add_subplot(111)
+    #set_trace()################
+    x=(pd.DataFrame(xPlot)).ix[:,0]
+    y=(pd.DataFrame(xPlot)).ix[:,2]
+    ax.scatter(x,y,s=30,c='green')
+    ax=plt.plot(x,y)    
+    plt.xlabel('x0');plt.ylabel('x2');
+    plt.show()
+
+    #x0/x3
+    fig=plt.figure() 
+    ax=fig.add_subplot(111)
+    #set_trace()################
+    x=(pd.DataFrame(xPlot)).ix[:,0]
+    y=(pd.DataFrame(xPlot)).ix[:,3]
+    ax.scatter(x,y,s=30,c='green')
+    ax=plt.plot(x,y)    
+    plt.xlabel('x0');plt.ylabel('x3');
+    plt.show()
