e34e5ad00c7917ec7b86ad6c17080dc4879534cf
282886636@qq.com <282886636@qq.com>
2016-07-11 19:25:52 +0800

    07111925

--- MachineLearning/ch06/svmMLiA.py
+++ MachineLearning/ch06/svmMLiA.py
@@ -8,6 +8,7 @@ def loadDataSet(fileName):
         lineArr=line.strip().split('\t')
         dataMat.append([float(lineArr[0]),float(lineArr[1])])
         labelMat.append(float(lineArr[2]))
+    return dataMat,labelMat
 
 
 def selectJrand(i,m):
@@ -20,6 +21,21 @@ def selectJrand(i,m):
 def clipAlpha(aj,H,L):
     if aj>H:
         aj=H
-    IF L>aj:
+    if L>aj:
         aj=L
     return aj
+
+
+def smoSimple(dataMatIn,classLabels,C,toler,maxIter):
+    dataMatrix=np.mat(dataMatIn);labelMat=np.mat(classLabels).transpose()
+    b=0;m,n=shape(dataMatrix)
+    alphas=np.mat(np.zeros((m,1)))
+    iter=0
+    while(iter<maxIter):
+        alphaPairsChanged=0
+        for i in range(m):
+            fxi=float(multiply(alphas,labelMat).T*(dataMatrix*dataMatrix[i,:].T))+b
+            Ei=fXi-float(labelMat[i])
+            alphaIold=alphas[i].copy();
+            alphaJold=alphas[j].copy();
+            
