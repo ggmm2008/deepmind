36bef083f898ff069e57667425baee728af8003e
282886636@qq.com <282886636@qq.com>
2016-07-21 22:22:37 +0800

    88

--- MachineLearning/ch10/kMeans.py
+++ MachineLearning/ch10/kMeans.py
@@ -2,6 +2,7 @@
 
 from numpy import *
 from ipdb import set_trace
+import matplotlib.pyplot as plt
 
 def loadDataSet(fileName):
     set_trace()
@@ -11,8 +12,8 @@ def loadDataSet(fileName):
         curLine=line.strip().split('\t')
         fltLine=map(float,curLine)
         dataMat.append(fltLine)
-    return dataMat
-
+    return mat(dataMat)
+    
 
 def distEclud(vecA,vecB):
     return sqrt(sum(power(vecA-vecB,2)))
@@ -27,8 +28,16 @@ def randCent(dataSet,k):
         centroids[:,j]=minJ+rangeJ*random.rand(k,1)
     return centroids
 
+#显示图标
+def plotData(centroids,dataMat):
+    plt.scatter(centroids[:,0],centroids[:,1],color='r')
+    plt.scatter(dataMat[:,0],dataMat[:,1])
+
+
+
 
 def kMeans(dataSet,k,distMeas=distEclud,createCent=randCent):
+    #set_trace()
     m=shape(dataSet)[0]
     clusterAssment=mat(zeros((m,2)))
     centroids=createCent(dataSet,k)
@@ -36,17 +45,55 @@ def kMeans(dataSet,k,distMeas=distEclud,createCent=randCent):
     while clusterchanged:
         clusterchanged=False
         for i in range(m):
+            #set_trace()
             minDist=inf;minIndex=-1
             for j in range(k):
                 distJI=distMeas(centroids[j,:],dataSet[i,:])
+                print i,j
                 if distJI<minDist:
                     minDist=distJI;minIndex=j
             if clusterAssment[i,0]!=minIndex:clusterchanged=True
             clusterAssment[i,:]=minIndex,minDist**2
         print centroids
         for cent in range(k):
+            #set_trace()
             ptsInClust=dataSet[nonzero(clusterAssment[:,0].A==cent)[0]]
             centroids[cent,:]=mean(ptsInClust,axis=0)
     return centroids,clusterAssment
     
+
+def biKmeans(dataSet,k,distMeas=distEclud):
+    #set_trace()
+    m=shape(dataSet)[0]
+    clusterAssment=mat(zeros((m,2)))
+    centroid0=mean(dataSet,axis=0).tolist()[0]
+    centList=[centroid0]
+    for j in range(m):
+        clusterAssment[j,1]=distMeas(mat(centroid0),dataSet[j,:])**2
+    set_trace()
+    while(len(centroid0)<k):
+        lowestSSE=inf
+        for i in range(len(centroid0)):
+            print "i:%d" % i
+            ptsInCurrCluster=dataSet[nonzero(clusterAssment[:,0].A==i)[0],:]
+            centroidMat,splitClustAss=kMeans(ptsInCurrCluster,2,distMeas)
+            sseSplit=sum(splitClustAss[:,1])
+            sseNotSplit=sum(clusterAssment[nonzero(clusterAssment[:,0].A!=i)[0],1])
+            print "sseSplit,and notSplit:",sseSplit,sseNotSplit
+            if(sseSplit+sseNotSplit)<lowestSSE:
+                bestCentToSplit=i
+                bestNewCents=centroidMat
+                bestClustAss=splitClustAss.copy()
+                lowestSSE=sseSplit+sseNotSplit
+        bestClustAss[nonzero(bestClustAss[:,0].A==1)[0],0]=len(centList)
+        bestClustAss[nonzero(bestClustAss[:,0].A==0)[0],0]=bestCentToSplit
+        print 'the bestCentToSplit is:',bestCentToSplit
+        print 'the len of bestClustAss is:',len(bestClustAss)
+        centList[bestCentToSplit]=bestNewCents[0,:]
+        centList.append(bestNewCents[1,:])
+        clusterAssment[nonzero(clusterAssment[:,0].A==bestCentToSplit)[0],:]=bestClustAss
+    
+    return mat(centList),clusterAssment
+
+
 
