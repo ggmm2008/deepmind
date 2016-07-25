7b56680df0517c6f88d6937668b6c07d351ac3f1
282886636@qq.com <282886636@qq.com>
2016-07-25 21:48:33 +0800

    55

--- MachineLearning/ch11/apriori.py
+++ MachineLearning/ch11/apriori.py
@@ -13,7 +13,7 @@ def loadDataSet():
 
 
 def createC1(dataSet):
-    set_trace()
+    #set_trace()
     C1=[]
     for transaction in dataSet:
         for item in transaction:
@@ -24,12 +24,12 @@ def createC1(dataSet):
 
 
 def scanD(D,CK,minSupport):
-    set_trace()
+    #set_trace()
     ssCnt={}
     for tid in D:
         for can in CK:  
             if can.issubset(tid):
-                if not sscnt.has_key(can):ssCnt[can]=1
+                if not ssCnt.has_key(can):ssCnt[can]=1
                 else:ssCnt[can]+=1
     numItems=float(len(D))
     retList=[]
@@ -39,6 +39,68 @@ def scanD(D,CK,minSupport):
         if support>=minSupport:
             retList.insert(0,key)
         supportData[key]=support
+    #set_trace()
     return retList,supportData
 
 
+def apriorGen(LK,k):#creates CK
+    retList=[]
+    lenLK=len(LK)
+    #set_trace()
+    for i in range(lenLK):
+        for j in range(i+1,lenLK):
+            L1=list(LK[i])[:k-2];L2=list(LK[j])[:k-2]
+            L1.sort();L2.sort()
+            if L1==L2:
+                retList.append(LK[i]|LK[j])
+    return retList
+
+
+def apriori(dataSet,minSupport=0.5):
+    C1=createC1(dataSet)
+    D=map(set,dataSet)
+    L1,supportData=scanD(D,C1,minSupport)
+    L=[L1]
+    k=2
+    while (len(L[k-2])>0):
+        CK=apriorGen(L[k-2],k)
+        LK,supK=scanD(D,CK,minSupport)
+        supportData.update(supK)
+        L.append(LK)
+        k+=1
+    return L,supportData
+
+
+def generateRules(L,supportData,minConf=0.7):
+    set_trace()
+    bigRuleList=[]
+    for i in range(1,len(L)):
+        for freqSet in L[i]:
+            H1=[frozenset([item]) for item in freqSet]
+            if(i>1):
+                rulesFromConseq(freqSet,H1,supportData,bigRuleList,minConf)
+            else:
+                calcConf(freqSet,H1,supportData,bigRuleList,minConf)
+        return bigRuleList
+
+
+def calcConf(freqSet,H,supportData,brl,minConf=0.7):
+    set_trace()
+    prunedH=[]
+    for conseq in H:
+        conf=supportData[freqSet]/supportData[freqSet-conseq]
+        if conf>=minConfL
+            print freqSet-conf,'-->',conseq,'conf:',conf
+            brl.append((freqSet-conseq,conseq,conf))
+            prunedH.append(conseq)
+    return prunedH
+
+
+def rulesFromConseq(freqSet,H,supportData,brl,minConf=0.7):
+    set_trace()
+    m=len(H[0])
+    if (len(freqSet)>(m+1)):
+        Hmp1=apriorGen(H,m+1)
+        Hmp1=calcConf(freqSet,Hmp1,supportData,brl,minConf)
+        if (len(Hmp1)>1):
+            rulesFromConseq(freqSet,Hmp1,supportData,brl,minConf)
