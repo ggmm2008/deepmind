739d4174a0def939bbea87713f49835c310288ff
282886636@qq.com <282886636@qq.com>
2016-08-14 20:55:58 +0800

    0814

--- MachineLearning/ch03/trees.py
+++ MachineLearning/ch03/trees.py
@@ -37,5 +37,19 @@ def splitDataSet(dataSet,axis,value):
 def chooseBestFeatureToSplit(dataSet):
     set_trace()
     numFeatures=len(dataSet[0])-1
-    baseEntropy=calcShannonEnt(dataSet)
-    
+    baseEntropy=calcShannonEnt(dataSet)#以标签类计算基础信息商
+    bestInfoGain=0.0;bestFeature=-1
+    for i in range(numFeatures):
+        featList=[example[i] for example in dataSet]
+        uniqueVals=set(featList)
+        newEntropy=0.0
+        for value in uniqueVals:
+            subDataSet=splitDataSet(dataSet,i,value)
+            prob=len(subDataSet)/float(len(dataSet))
+            newEntropy+=prob*calcShannonEnt(subDataSet)
+        infoGain=baseEntropy-newEntropy
+        if(infoGain>bestInfoGain):
+            bestInfoGain=infoGain
+            bestFeature=i
+    return bestFeature
+
