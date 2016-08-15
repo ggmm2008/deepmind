4ff6d012f1b016a417e0b0b9466af08ddee6e317
282886636@qq.com <282886636@qq.com>
2016-08-15 21:57:26 +0800

    0815

--- MachineLearning/ch03/treePlot.py
+++ MachineLearning/ch03/treePlot.py
@@ -25,10 +25,11 @@ def createPlot():
 
 
 def getNumLeafs(myTree):
+    #set_trace()
     numLeafs=0
     fisrtStr=myTree.keys()[0]
     secondDict=myTree[fisrtStr]
-    for key in secondDict.key():
+    for key in secondDict.keys():
         if type(secondDict[key]).__name__=='dict':
             numLeafs+=getNumLeafs(secondDict[key])
         else:numLeafs+=1
@@ -37,12 +38,56 @@ def getNumLeafs(myTree):
 
 def getTreeDepth(myTree):
     maxDepth=0
-    firstStr=myTree.key()[0]
+    firstStr=myTree.keys()[0]
     secondDict=myTree[firstStr]
-    for key in secondDict.key():
+    for key in secondDict.keys():
         if type(secondDict[key]).__name__=='dict':
             thisDepth=1+getTreeDepth(secondDict[key])
-        else thisDepth=1
+        else: thisDepth=1
         if thisDepth>maxDepth:maxDepth=thisDepth
     return maxDepth
             
+
+def retrieveTree(i):
+    listOfTree=[{'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},\
+                {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head':{0:'no',1:'yes'}}\
+                , 1: 'yes'}}}}]
+    return listOfTree[i]
+
+
+def plotMidText(cntrPt,parentPt,txtString):
+    xMid=(parentPt[0]-cntrPt[0])/2.0+cntrPt[0]
+    yMid=(parentPt[1]-cntrPt[1])/2.0+cntrPt[1]
+    createPlot.ax1.text(xMid,yMid,txtString)
+
+
+def plotTree(myTree,parentPt,nodeTxt):
+    numLeafs=getNumLeafs(myTree)
+    depth=getTreeDepth(myTree)
+    firstStr=myTree.keys()[0]
+    cntrPt=(plotTree.xoff+(1.0+float(numLeafs))/2.0/plotTree.totalW,plotTree.yoff)
+    plotMidText(cntrPt,parentPt,nodeTxt)
+    plotNode(firstStr,cntrPt,parentPt,decisionNode)
+    secondDict=myTree[firstStr]
+    plotTree.yoff=plotTree.yoff-1.0/plotTree.totalD
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__=='dict':
+            plotTree(secondDict[key],cntrPt,str(key))
+        else:
+            plotTree.xoff=plotTree.xoff+1.0/plotTree.totalW
+            plotNode(secondDict[key],(plotTree.xoff,plotTree.yoff),cntrPt,leafNode)
+            plotMidText((plotTree.xoff,plotTree.yoff),cntrPt,str(key))
+    plotTree.yoff=plotTree.yoff+1.0/plotTreee.totalD
+
+
+
+def createPlot(inTree):
+    fig=plt.figure(1,facecolor='white')
+    fig.clf()
+    axprops=dict(xticks=[],yticks=[])
+    createPlot.ax1=plt.subplot(111,frameon=False,**axprops)
+    plotTree.totalW=float(getNumLeafs(inTree))
+    plotTree.totalD=float(getTreeDepth(inTree))
+    plotTree.xoff=-0.5/plotTree.totalW;plotTree.yoff=1.0;
+    plotTree(inTree,(0.5,1.0),'')
+    plt.show()
