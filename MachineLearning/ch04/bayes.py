2142f733bdb46c9577e2cd1c22900605394676a2
282886636@qq.com <282886636@qq.com>
2016-08-25 20:00:27 +0800

    0825

--- MachineLearning/ch04/bayes.py
+++ MachineLearning/ch04/bayes.py
@@ -34,6 +34,17 @@ def setOfWords2Vec(vocabList, inputSet):
         else: print "the word: %s is not in my Vocabulary!" % word
     return returnVec
 
+
+def bagOfWords2Vec(vocabList, inputSet):
+    #set_trace()
+    returnVec = [0]*len(vocabList)
+    for word in inputSet:
+        if word in vocabList:
+            returnVec[vocabList.index(word)] += 1
+        else: print "the word: %s is not in my Vocabulary!" % word
+    return returnVec
+
+
 def trainNB0(trainMatrix,trainCategory):
     #set_trace()
     numTrainDocs = len(trainMatrix)
@@ -49,7 +60,7 @@ def trainNB0(trainMatrix,trainCategory):
         else:
             p0Num += trainMatrix[i]
             p0Denom += sum(trainMatrix[i])
-    set_trace()
+    #set_trace()
     p1Vect = np.log(p1Num/p1Denom)         #change to log()
     p0Vect = np.log(p0Num/p0Denom)        #change to log()
     return p0Vect,p1Vect,pAbusive
@@ -60,3 +71,26 @@ def listVec(vocabList,inputSet):
     for postinDoc in inputSet:
         trainMat.append(setOfWords2Vec(vocabList,postinDoc))
     return trainMat
+
+def classifyNb(vec2Classify,p0Vec,p1Vec,pClass1):
+    p1=sum(vec2Classify*p1Vec)+np.log(pClass1)
+    p0=sum(vec2Classify*p0Vec)+np.log(1-pClass1)
+    if p1>p0:
+        return 1
+    else:
+        return 0
+
+
+def testingNB():
+    dataSet,listClasses=loadDataSet()
+    myVocabList=createVocabList(dataSet)
+    trainMat=[]
+    for line in dataSet:
+        trainMat.append(setOfWords2Vec(myVocabList,line))
+    p0V,p1V,pAb=trainNB0(trainMat,listClasses)
+    testEntry=['love','my','dalmation']
+    thisDoc=np.array(setOfWords2Vec(myVocabList,testEntry))
+    print testEntry,'classifyed as :',classifyNb(thisDoc,p0V,p1V,pAb)
+    testEntry=['stupid','garbage']
+    thisDoc=np.array(setOfWords2Vec(myVocabList,testEntry))
+    print testEntry,'classifyed as :',classifyNb(thisDoc,p0V,p1V,pAb)
